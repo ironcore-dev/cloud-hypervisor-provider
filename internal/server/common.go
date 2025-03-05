1ba35c8583c3390acebbee5b64c67a3c7f67528c
Lukas Frank <lukas.frank@sap.com>
2025-03-05 10:53:26 +0100

    Scaffold

---
+++ internal/server/common.go
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2023 SAP SE or an SAP affiliate company and IronCore contributors
+// SPDX-License-Identifier: Apache-2.0
+
+package server
+
+import (
+	"context"
+
+	"github.com/go-logr/logr"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+func (s *Server) loggerFrom(ctx context.Context, keysWithValues ...interface{}) logr.Logger {
+	return ctrl.LoggerFrom(ctx, keysWithValues...)
+}
